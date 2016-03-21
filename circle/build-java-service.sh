6f59b977dcc3987d2d7b826e45eaa0fd2dbf6495
Joe Barnett <jbarnett@womply.com>
2016-03-21 14:25:03 -0700

    Add back COVERALLS_REPO_TOKEN doc

--- circle/build-java-service.sh
+++ circle/build-java-service.sh
@@ -5,6 +5,7 @@
 # Non Circle Provided Input Variables:
 #   MVN_ARGS: extra arguments to pass to maven
 #   DOCKER_HOST: maven uses this to connect to the docker daemon, generally unix:///var/run/docker.sock
+#   COVERALLS_REPO_TOKEN: coveralls API token for the current repository, used by maven coveralls plugin
 
 set -e
 
