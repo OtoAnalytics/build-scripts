41657b775998f222a2755a2d7921768230c0155a
Joe Barnett <jbarnett@womply.com>
2016-07-21 13:17:37 -0700

    [Task] allow SLACK_CHANNEL env override to slack notification

--- circle/do-slack-notification.sh
+++ circle/do-slack-notification.sh
@@ -10,5 +10,9 @@
 set +e
 
 if [ "${SLACK_NOTIFICATION_WEBHOOK}" != "" ]; then
-  curl -X POST -d "payload={\"username\":\"Github\", \"icon_emoji\":\":octocat:\", \"attachments\":[{ \"mrkdwn_in\":[\"text\"], \"color\":\"#439FE0\", \"fallback\":\"Merge Notification\", \"pretext\":\"Merge Notification\", \"text\":\"*Project* \`${CIRCLE_PROJECT_REPONAME}\`\n*Branch* \`${CIRCLE_BRANCH}\`\n*Version* \`${CURRENT_VERSION}\`\n*Docker Image* \`${SPECIFIC_BRANCH}\`\nClick <$CIRCLE_COMPARE_URL|here> to see what changed\"}]}" ${SLACK_NOTIFICATION_WEBHOOK}
+  if [ "${SLACK_CHANNEL}" != "" ]; then
+    curl -X POST -d "payload={\"username\":\"Github\", \"channel\":\"${SLACK_CHANNEL}\", \"icon_emoji\":\":octocat:\", \"attachments\":[{ \"mrkdwn_in\":[\"text\"], \"color\":\"#439FE0\", \"fallback\":\"Merge Notification\", \"pretext\":\"Merge Notification\", \"text\":\"*Project* \`${CIRCLE_PROJECT_REPONAME}\`\n*Branch* \`${CIRCLE_BRANCH}\`\n*Version* \`${CURRENT_VERSION}\`\n*Docker Image* \`${SPECIFIC_BRANCH}\`\nClick <$CIRCLE_COMPARE_URL|here> to see what changed\"}]}" ${SLACK_NOTIFICATION_WEBHOOK}
+  else
+    curl -X POST -d "payload={\"username\":\"Github\", \"icon_emoji\":\":octocat:\", \"attachments\":[{ \"mrkdwn_in\":[\"text\"], \"color\":\"#439FE0\", \"fallback\":\"Merge Notification\", \"pretext\":\"Merge Notification\", \"text\":\"*Project* \`${CIRCLE_PROJECT_REPONAME}\`\n*Branch* \`${CIRCLE_BRANCH}\`\n*Version* \`${CURRENT_VERSION}\`\n*Docker Image* \`${SPECIFIC_BRANCH}\`\nClick <$CIRCLE_COMPARE_URL|here> to see what changed\"}]}" ${SLACK_NOTIFICATION_WEBHOOK}
+  fi
 fi
