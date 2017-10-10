092570a6be755d8b971efb7dc5e6e3b95af738d7
Zee Alexander <zee@zee.space>
2017-10-10 16:39:55 -0700

    added stat for debugging

--- circle/setup-ruby-service.sh
+++ circle/setup-ruby-service.sh
@@ -14,21 +14,25 @@ declare -rx SCRIPT=${0##*/}
 declare -rx BUILDSCRIPTS="${HOME}/build-scripts/circle"
 
 # Required Items
+
 declare -rx stop_competitors="${BUILDSCRIPTS}/stop-competitors.sh"
 declare -rx setup_docker="${BUILDSCRIPTS}/setup-docker.sh"
 declare -rx setup_ruby="${BUILDSCRIPTS}/setup-ruby.sh"
 
-if test ! -x "$setup_docker"; then
-    printf "$SCRIPT:$LINENO: the command %s is not available - aborting\n" "$setup_docker" >&2
-    exit 192
-fi
-
 if test ! -x "$stop_competitors"; then
+    stat $stop_competitors
     printf "$SCRIPT:$LINENO: the command %s is not available - aborting\n" "$stop_competitors" >&2
     exit 192
 fi
 
+if test ! -x "$setup_docker"; then
+    stat $setup_docker
+    printf "$SCRIPT:$LINENO: the comma2nd %s is not available - aborting\n" "$setup_docker" >&2
+    exit 192
+fi
+
 if test ! -x "$setup_ruby"; then
+    stat $setup_ruby
     printf "$SCRIPT:$LINENO: the command %s is not available - aborting\n" "$setup_docker" >&2
     exit 192
 fi
