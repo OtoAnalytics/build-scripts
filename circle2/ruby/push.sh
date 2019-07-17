519f0f1ed18c84d6d73aa7d236b7f2db6c808212
Andrew Nguyen <andrew13nguyen@gmail.com>
2019-07-17 12:59:18 -0700

    update docker login to use --password-stdin

--- circle2/ruby/push.sh
+++ circle2/ruby/push.sh
@@ -4,6 +4,6 @@ DOCKER_IMAGE_TAG=${1:-'latest'}
 DOCKER_REGISTERY='quay.io/womply'
 
 echo " - Pushing $REPO_NAME:$DOCKER_IMAGE_TAG to $DOCKER_REGISTERY"
-docker login -u $DOCKER_USER -p $DOCKER_PASS quay.io
+echo $DOCKER_PASS | docker login --username $DOCKER_USER --password-stdin quay.io
 docker build --build-arg GEMFURY_DEPLOY_TOKEN -t $DOCKER_REGISTERY/$REPO_NAME:$DOCKER_IMAGE_TAG .
 docker push $DOCKER_REGISTERY/$REPO_NAME:$DOCKER_IMAGE_TAG
