513ddffb86f1b4848e6e286b67821fde130627c9
Kohitij Das <kdas@womply.com>
2021-03-02 18:24:10 +0530

    [DK-4965] Update deploy-java-service.sh script to conditionally handle deployment for both ECS & EKS

--- circle2/deploy-java-service.sh
+++ circle2/deploy-java-service.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Does a deploy to ECS, sets up tags & release notes in github, and notifies slack
+# Does a deploy to either ECS or EKS, sets up tags & release notes in github, and notifies slack
 #
 # Non Circle Provided Input Variables:
 #   REPO_ROOT: URL to the docker repository organization (eg quay.io/womply)
@@ -11,12 +11,32 @@
 
 set -e
 
+# Populate the necessary variables
 export CURRENT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
 TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
 export SPECIFIC_BRANCH=${TIMESTAMP}-${CIRCLE_BRANCH}-${CIRCLE_SHA1:0:7}
+MANIFESTS_GITHUB_REPO='OtoAnalytics/microservice-manifests'
+MANIFESTS_BRANCH="master"
+MANIFESTS_DIR="manifests-repo"
+if [[ "${CIRCLE_BRANCH}" == 'master' ]]; then
+  ECS_CLUSTER=$ECS_PROD_CLUSTER
+  EKS_ENVIRONMENT='prod'
+else
+  ECS_CLUSTER=$ECS_PREPROD_CLUSTER
+  EKS_ENVIRONMENT='beta'
+fi
 
-do-ecs-deployment.sh
+# Deploy to ECS if the service exists in ECS
+if [[ ! `aws ecs list-services --cluster ${ECS_CLUSTER} | grep $CIRCLE_PROJECT_REPONAME | wc -l` -eq 0 ]]; then
+  do-ecs-deployment.sh
+fi
 
+# Deploy to EKS if the service config exists in the microservice manifests
+[[ -d ${MANIFESTS_DIR} ]] || git clone --single-branch --branch ${MANIFESTS_BRANCH} https://${WOMPLY_CIRCLECI_SHARED_USER_GITHUB_ACCESS_TOKEN}@github.com/${MANIFESTS_GITHUB_REPO}.git ${MANIFESTS_DIR}
+if [[ -f "${MANIFESTS_DIR}/src/environments/${EKS_ENVIRONMENT}/image-tags/${CIRCLE_PROJECT_REPONAME}.yaml" ]]; then
+  do-eks-deployment.sh ${SPECIFIC_BRANCH} ${EKS_ENVIRONMENT}
+fi
+
+# Execute post-deployment tasks
 do-release-notes.sh
-
 do-slack-notification.sh
