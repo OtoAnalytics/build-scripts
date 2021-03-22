bfa3d01c7b7b698c4dd9999e3db02d5632669ce8
Pepelo <nlehlou@gmail.com>
2021-03-22 15:33:52 -0500

    [DK-4965] Fix EKS deployment bug

--- circle2/deploy-java-service-to-eks.sh
+++ circle2/deploy-java-service-to-eks.sh
@@ -5,11 +5,7 @@
 set -e
 
 export CURRENT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
-TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
-export SPECIFIC_BRANCH=${TIMESTAMP}-${CIRCLE_BRANCH}-${CIRCLE_SHA1:0:7}
 
-do-eks-deployment.sh ${SPECIFIC_BRANCH} ${EKS_ENVIRONMENT}
-
+do-eks-deployment.sh ${CIRCLE_BRANCH} ${EKS_ENVIRONMENT}
 do-release-notes.sh
-
 do-slack-notification.sh
